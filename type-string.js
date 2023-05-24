a91eb3654806b29622a3ef654f9867e582debc44
Demetrius Vignati <demetriusvas@gmail.com>
2023-05-24 18:21:38 -0300

    aula 1

---
+++ type-string.js
@@ -0,0 +1,15 @@
+const texto1 = "Olá, mundo!";
+const texto2 = 'Olá, mundo! Sem aspas';
+const senha = "senhaSuperSegura12345678";
+const stringDeNumeros = "12345678";
+
+console.log(texto1);
+console.log(texto2);
+console.log(senha);
+console.log(stringDeNumeros);
+
+const citacao = "Meu nome é ";
+const meuNome = "Demetrius";
+
+//Concatenação
+console.log(citacao + meuNome);
